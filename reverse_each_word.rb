6d006ef5c5d954120e0f0fcad05d6297924d6f53
Alehandro Persaud <andrew041994@gmail.com>
2020-01-23 16:41:49 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(sentence1)
   array = sentence1.split
   reversed_words = []
-  array.map { |str| reversed_words << str.reverse }
+  array.collect { |str| reversed_words << str.reverse }
   reversed_words.join(" ")
 end
