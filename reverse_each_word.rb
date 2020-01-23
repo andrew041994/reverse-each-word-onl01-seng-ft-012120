da2c4d5ccf9af306ec81d42c5dd5e4276074ea11
Alehandro Persaud <andrew041994@gmail.com>
2020-01-23 16:31:54 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(sentence1)
   array = sentence1.split
   reversed_words = []
   i=0
-  while i < reversed_words.size
+  while i < array.size
     reversed_words << array[i].reverse
     i+=1
   end
